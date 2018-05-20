40657823830be09326f927888d7bf6c78271b471
Ankur Konar <ankur.bidhannagar@gmail.com>
2018-05-20 20:03:30 +0000

    Added captcha in signup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express =  require("express");
 var router = express.Router({mergeParams: true});
 var passport = require("passport");
+var request = require("request");
 var Campground = require("../models/campgrounds");
 var Comment = require("../models/comment");
 var User = require("../models/user");
+
 
 
 //Route to the landing page
@@ -26,19 +28,37 @@ router.get("/register", function(req, res){
 });
 
 router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    if(req.body.adminCode == "secretcode123"){
-        newUser.isadmin = true;
+    const captcha = req.body["g-recaptcha-response"];
+    if (!captcha) {
+      console.log(req.body);
+      req.flash("error", "Please select captcha");
+      return res.redirect("/register");
     }
-    User.register( newUser, req.body.password, function(err, user){
-        if(err){
-            req.flash("error", err.message);
-            return res.redirect("/register");
+     // secret key
+    var secretKey = process.env.CAPTCHA;
+    // Verify URL
+    var verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${req.connection.remoteAddress}`;
+    // Make request to Verify URL
+    request.get(verifyURL, (err, response, body) => {
+      // if not successful
+      if (body.success !== undefined && !body.success) {
+        req.flash("error", "Captcha Failed");
+        return res.redirect("/register");
+      }
+        var newUser = new User({username: req.body.username});
+        if(req.body.adminCode == "secretcode123"){
+            newUser.isadmin = true;
         }
-        passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome to yelpcamp, " + user.username + ".");
-            res.redirect("/campgrounds");
-        })
+        User.register( newUser, req.body.password, function(err, user){
+            if(err){
+                req.flash("error", err.message);
+                return res.redirect("/register");
+            }
+            passport.authenticate("local")(req, res, function(){
+                req.flash("success", "Welcome to yelpcamp, " + user.username + ".");
+                res.redirect("/campgrounds");
+            });
+        });
     });
 });
 
